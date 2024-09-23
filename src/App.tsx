e2473c1cac23b4f3585000aebd24d11b87069214
abudsistem <abudsistem@gmail.com>
2024-09-22 21:26:57 -0500

    FSO-part2-b Filtrado de elementos mostrados

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ const App = (props) => {
   const [newNote, setNewNote] = useState(
     'a new note'
   )
-
+  const [showAll,setShowAll] = useState(false)
+  
 
   const addNote = (event) => {
     event.preventDefault()
@@ -25,11 +26,20 @@ const App = (props) => {
     setNewNote(event.default.value)
   }
 
+  const notesToShow = showAll 
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={()=> setShowAll(!showAll)}>
+          show {showAll ? 'important': 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note}/>
         )}
       </ul>
