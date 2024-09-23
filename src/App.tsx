885a7660560ada0688a3fd4a6b7a5a842f462de6
abudsistem <abudsistem@gmail.com>
2024-09-22 21:14:26 -0500

    FSO part2 b componentes controlados

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react'
+
 import Note from './components/Note'
-const App = ({notes}) => {
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note'
+  )
+
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() >0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) =>{
+    setNewNote(event.default.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -8,6 +33,13 @@ const App = ({notes}) => {
           <Note key={note.id} note={note}/>
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type='submit'> save </button>
+      </ form>
     </div>
   )
 }
