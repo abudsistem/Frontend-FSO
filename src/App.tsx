8137695dc8948a26f5de805d51d9b1021a588975
abudsistem <abudsistem@gmail.com>
2024-09-26 19:41:40 -0500

    FSO-part2-d Extraer la comunicacion a un modulo separado, Sintaxis mas limpia para definir objetos literales ademas de promesas y errores

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -8,11 +9,17 @@ const App = () => {
   const [showAll,setShowAll] = useState(false)
 
   useEffect(()=> {
-    axios
-      .get('http://localhost:3001/notes')
-      .then( response => {
-        setNotes(response.data)
-      })
+    //axios
+    noteService
+      //.get('http://localhost:3001/notes')
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      }
+      )
+      //.then( response => {
+      //  setNotes(response.data)
+      //})
   },[])
 
   const addNote = (event) => {
@@ -22,13 +29,40 @@ const App = () => {
       important: Math.random() >0.5,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then( response => {
-        setNotes(notes.concat(response.data))
-        setNewNote('')
+    //axios
+    //  .post('http://localhost:3001/notes', noteObject)
+    //  .then( response => {
+    //    setNotes(notes.concat(response.data))
+    //    setNewNote('')
+    //  })
+    noteService
+      .create(noteObject)
+        .then(returnedNote =>{
+          setNotes(notes.concat(returnedNote))
+          setNewNote('')
+        }
+
+        )
+  }
+
+   const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+        .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
+
 
   const handleNoteChange = (event) =>{
     setNewNote(event.target.value)
@@ -48,7 +82,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note}/>
+          <Note 
+            key={note.id} 
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
---
+++ src/services/notes.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+/*
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request
+    .then(response => response.data)
+}
+*/
+const getAll = ()=>{
+  const request = axios.get(baseUrl)
+  const nonExisting = {
+    id: 1000,
+    content: "this note not exist",
+    important: true,
+  }
+  return request.then(response => response.data.concat(nonExisting))
+}
+
+const create = newObject => {
+//  return axios.post(baseUrl, newObject)
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { 
+getAll, create, update
+//  create: create, 
+//  update: update 
+//  getAll: getAll, 
+}
